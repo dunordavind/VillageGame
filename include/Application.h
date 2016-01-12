d5341aaabe9f9b339bc1abdd7f5bb214b9178ee4
dunordavind@gmail.com <the123to098!>
2016-01-12 13:15:24 +0100

    Added application class

---
+++ include/Application.h
@@ -0,0 +1,17 @@
+//
+// Created by Petr Volkov on 12/01/2016.
+//
+
+#ifndef VILLAGEGAME_APPLICATION_H
+#define VILLAGEGAME_APPLICATION_H
+
+namespace Sus {
+    class Application {
+    public:
+        Application();
+        ~Application();
+    };
+}
+
+
+#endif //VILLAGEGAME_APPLICATION_H
---
+++ src/Application.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Petr Volkov on 12/01/2016.
+//
+
+#include "Application.h"
+
+namespace Sus {
+    Application::Application() {
+
+    }
+
+    Application::~Application() {
+
+    }
+}
