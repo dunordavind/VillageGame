ce0f29b79850d71e51903f1ddfde18317b00facb
dunordavind@gmail.com <the123to098!>
2016-01-12 13:29:02 +0100

    main function creates and deletes application class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Application.h"
 
 using namespace std;
 
 int main() {
+    //initialize an application class
+    village::Application *a = new(std::nothrow) village::Application();
+
+    //delete application class
+    delete(a);
+    a = NULL;
 
     return 0;
 }
