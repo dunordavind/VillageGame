223e647efbb901858c649fbe608870e03191ffcd
dunordavind@gmail.com <the123to098!>
2016-01-13 23:30:56 +0100

    Main loop gets called in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main() {
     //initialize an application class
     village::Application *a = new(std::nothrow) village::Application();
 
+    int result = a->run();
+
     //delete application class
     delete(a);
     a = NULL;
