d94d7c554a8293b21f88a0af5a086277917586c9
dunordavind@gmail.com <the123to098!>
2016-01-17 17:10:55 +0100

    Added typeEntityId Entity now have an id member variable

--- include/Entity.h
+++ include/Entity.h
@@ -5,9 +5,12 @@
 #ifndef VILLAGEGAME_ENTITY_H
 #define VILLAGEGAME_ENTITY_H
 
+#include "EntityTypes.h"
+
 namespace village {
     class Entity {
-
+    private:
+         entityIdType entityId;
     };
 }
 
---
+++ include/EntityTypes.h
@@ -0,0 +1,14 @@
+//
+// Created by Petr Volkov on 17/01/2016.
+//
+
+#ifndef VILLAGEGAME_ENTITYTYPES_H
+#define VILLAGEGAME_ENTITYTYPES_H
+
+#include "Config.h"
+
+namespace village {
+    typedef Uint32 entityIdType;
+}
+
+#endif //VILLAGEGAME_ENTITYTYPES_H
