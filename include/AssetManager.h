7e642e7148dd4239d429fd6052160da9a4f843da
dunordavind@gmail.com <the123to098!>
2016-01-19 17:04:36 +0100

    Implemented AssetManager class

--- include/AssetManager.h
+++ include/AssetManager.h
@@ -5,10 +5,48 @@
 #ifndef VILLAGEGAME_ASSETMANAGER_H
 #define VILLAGEGAME_ASSETMANAGER_H
 
+#include <map>
+#include <iostream>
+#include <typeinfo>
+#include <templates/TAssetHandler.h>
+#include "AssetTypes.h"
+#include "AssetHandler.h"
 
-class AssetManager {
+namespace village {
+    class AssetManager {
+    public:
 
-};
+        AssetManager();
+
+        virtual ~AssetManager();
+
+        void registerHandler(AssetHandler *handler);
+
+        bool loadAllAssets();
+
+        template<typename T>
+        TAssetHandler<T> &getHandler() const {
+            TAssetHandler<T> *result = nullptr;
+
+            std::map<const assetHandlerIdType, AssetHandler *>::const_iterator it = handlers.find(typeid(T).name());
+
+            if (it != handlers.end()) {
+                result = static_cast<TAssetHandler<T> *>(it->second);
+            }
+
+            if(result == nullptr) {
+                //change to logger after logger implemeneted
+                std::cerr << " AssetManager::getHandler( " << typeid(T).name() << " ) not found" << std::endl;
+            }
+
+            return *result;
+        }
 
 
+    private:
+        std::map<const assetHandlerIdType, AssetHandler *> handlers;
+        AssetManager(const AssetManager&);
+        AssetManager& operator=(const AssetManager&);
+    };
+}
 #endif //VILLAGEGAME_ASSETMANAGER_H
--- src/AssetManager.cpp
+++ src/AssetManager.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "AssetManager.h"
+
+namespace village {
+
+    AssetManager::AssetManager() {
+
+    }
+
+    AssetManager::~AssetManager() {
+        std::map<const assetHandlerIdType, AssetHandler *>::iterator it;
+        for(it = handlers.begin(); it != handlers.end(); it++) {
+            AssetHandler *handler = it->second;
+            handlers.erase(it);
+            delete handler;
+        }
+    }
+
+    void AssetManager::registerHandler(AssetHandler *handler) {
+        std::map<const assetHandlerIdType, AssetHandler *>::iterator it;
+
+        if(handler != nullptr) {
+            it = handlers.find(handler->getId());
+            if(it == handlers.end()) {
+                handlers.insert(std::pair<const assetHandlerIdType, AssetHandler *>(handler->getId(), handler));
+            } else {
+                std::cerr << " AssetManager::registerHandler( " << handler->getId() << " ): handler with this id already registered" << std::endl;
+            }
+        } else {
+            std::cerr << " AssetManager::registerHandler( NULL ): trying to insert a NULL handler" << std::endl;
+        }
+    }
+
+    bool AssetManager::loadAllAssets() {
+        bool result = true;
+        std::map<const assetHandlerIdType, AssetHandler *>::iterator it;
+
+        for(it = handlers.begin(); it != handlers.end(); it++) {
+            result = result && it->second->loadAllAssets();
+        }
+        return result;
+    }
+
+}
